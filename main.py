dded2b86470fc73a0f5a6f3b44e29454ba005e87
Mover <movermercene@gmail.com>
2024-05-04 22:23:22 +0800

    4.05.24

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="1234",
+  database="mydatabase"
 )
 
+
 mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE DB1")  # Create a database named TestDB
-
-mycursor.execute("SHOW DATABASES")  # Show all databases
-
-for db in mycursor:
-  print(db)  # Print all databases
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
